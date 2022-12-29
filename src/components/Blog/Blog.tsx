ee6e3f7ea95140efa1afcc4a1f079a4e104fedf9
mdsadikul <mdsadikulislam891@gmail.com>
2022-12-29 19:22:22 +0600

    added some questions

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,14 +1,103 @@
 import './Blog.css'
-import image from '../../assets/ball_prev_ui.png'
+
 
 const Blog = () => {
     return (
         <div className="container">
             <h1>This is Blog</h1>
             <div className='blog'>
-                <h3>Blog No-1</h3>
-                <img src={image} alt="" />
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo labore minus odit rem, iusto itaque voluptatem voluptatibus magni deserunt mollitia cum, atque impedit, quis officia inventore non consequatur quaerat. Labore voluptatem dolor temporibus, doloremque fugiat ipsum rem sed eum sequi eveniet deleniti, aperiam quam corrupti pariatur molestias! Ea corporis minus sit qui! Labore, dolore sunt ad itaque ex eligendi cumque? Eligendi exercitationem deleniti autem sint possimus assumenda nobis alias vitae iure ducimus voluptatibus hic enim, nulla sed laboriosam non accusantium asperiores magni modi! Fuga minus aut doloremque sunt exercitationem tempore autem recusandae aspernatur quos, aperiam pariatur modi expedita dignissimos, accusantium molestiae est totam ratione similique facere aliquam culpa provident dolore repudiandae omnis? Sit fugiat in nesciunt corrupti porro architecto aperiam error. Nam, inventore repellendus. Voluptatum odio minima suscipit perspiciatis ratione earum dolorem rerum error. Facere iusto quam enim cumque illum non maxime libero molestiae aliquam neque eos animi voluptatem placeat, laboriosam itaque recusandae praesentium maiores. Voluptatem dolorem accusamus veritatis dolorum iusto et officiis ullam temporibus corporis perferendis,</p>
+                <div>
+                    <h3>HTML 15 Questions and Answers</h3>
+                    <div className='question'>
+                        <p><strong>Que-1: What is HTML?</strong></p>
+                        <p><strong>Ans: </strong>HTML is a hypertext markup language. Primarily used to develop web pages or applications.</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-2: What is the difference between HTML tags and attributes?</strong></p>
+                        <p><strong>Ans: </strong>Tag and attribute are two concepts related to HTML. The main difference between tag and attribute is - tag is a way of representing an HTML element in the program, Attribute- An attribute is a way to describe the characteristics of an HTML element.</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-3: Difference between html and html5?</strong></p>
+                        <p><strong>Ans: </strong>Both HTML and HTML5 are hypertext markup languages, primarily used to develop web pages or applications. HTML5- is the latest version of HTML. HTML5 also supports audio and video, JavaScript can be run in the background. HTML- No audio and video support.</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-4: What are Marquee tags?</strong></p>
+                        <p><strong>Ans: </strong>Marquee is a function, to add scrollable text/img on your web page. "marquee" ... "marquee".</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-5: What is the “class” attribute in HTML?</strong></p>
+                        <p><strong>Ans: </strong>The class attribute is used to specify the class name for an HTML element. Multiple elements in HTML can have the same class value. Also, it is mainly used to associate the styles written in the stylesheet with the HTML elements.</p>
+                    </div>
+                </div>
+                <div>
+                    <h3>HTML 15 Questions and Answers</h3>
+                    <div className='question'>
+                        <p><strong>Que-1: What is HTML?</strong></p>
+                        <p><strong>Ans: </strong>HTML is a hypertext markup language. Primarily used to develop web pages or applications.</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-2: What is the difference between HTML tags and attributes?</strong></p>
+                        <p><strong>Ans: </strong>Tag and attribute are two concepts related to HTML. The main difference between tag and attribute is - tag is a way of representing an HTML element in the program, Attribute- An attribute is a way to describe the characteristics of an HTML element.</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-3: Difference between html and html5?</strong></p>
+                        <p><strong>Ans: </strong>Both HTML and HTML5 are hypertext markup languages, primarily used to develop web pages or applications. HTML5- is the latest version of HTML. HTML5 also supports audio and video, JavaScript can be run in the background. HTML- No audio and video support.</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-4: What are Marquee tags?</strong></p>
+                        <p><strong>Ans: </strong>Marquee is a function, to add scrollable text/img on your web page. "marquee" ... "marquee".</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-5: What is the “class” attribute in HTML?</strong></p>
+                        <p><strong>Ans: </strong>The class attribute is used to specify the class name for an HTML element. Multiple elements in HTML can have the same class value. Also, it is mainly used to associate the styles written in the stylesheet with the HTML elements.</p>
+                    </div>
+                </div>
+                <div>
+                    <h3>HTML 15 Questions and Answers</h3>
+                    <div className='question'>
+                        <p><strong>Que-1: What is HTML?</strong></p>
+                        <p><strong>Ans: </strong>HTML is a hypertext markup language. Primarily used to develop web pages or applications.</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-2: What is the difference between HTML tags and attributes?</strong></p>
+                        <p><strong>Ans: </strong>Tag and attribute are two concepts related to HTML. The main difference between tag and attribute is - tag is a way of representing an HTML element in the program, Attribute- An attribute is a way to describe the characteristics of an HTML element.</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-3: Difference between html and html5?</strong></p>
+                        <p><strong>Ans: </strong>Both HTML and HTML5 are hypertext markup languages, primarily used to develop web pages or applications. HTML5- is the latest version of HTML. HTML5 also supports audio and video, JavaScript can be run in the background. HTML- No audio and video support.</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-4: What are Marquee tags?</strong></p>
+                        <p><strong>Ans: </strong>Marquee is a function, to add scrollable text/img on your web page. "marquee" ... "marquee".</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-5: What is the “class” attribute in HTML?</strong></p>
+                        <p><strong>Ans: </strong>The class attribute is used to specify the class name for an HTML element. Multiple elements in HTML can have the same class value. Also, it is mainly used to associate the styles written in the stylesheet with the HTML elements.</p>
+                    </div>
+                </div>
+                <div>
+                    <h3>HTML 15 Questions and Answers</h3>
+                    <div className='question'>
+                        <p><strong>Que-1: What is HTML?</strong></p>
+                        <p><strong>Ans: </strong>HTML is a hypertext markup language. Primarily used to develop web pages or applications.</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-2: What is the difference between HTML tags and attributes?</strong></p>
+                        <p><strong>Ans: </strong>Tag and attribute are two concepts related to HTML. The main difference between tag and attribute is - tag is a way of representing an HTML element in the program, Attribute- An attribute is a way to describe the characteristics of an HTML element.</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-3: Difference between html and html5?</strong></p>
+                        <p><strong>Ans: </strong>Both HTML and HTML5 are hypertext markup languages, primarily used to develop web pages or applications. HTML5- is the latest version of HTML. HTML5 also supports audio and video, JavaScript can be run in the background. HTML- No audio and video support.</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-4: What are Marquee tags?</strong></p>
+                        <p><strong>Ans: </strong>Marquee is a function, to add scrollable text/img on your web page. "marquee" ... "marquee".</p>
+                    </div>
+                    <div className='question'>
+                        <p><strong>Que-5: What is the “class” attribute in HTML?</strong></p>
+                        <p><strong>Ans: </strong>The class attribute is used to specify the class name for an HTML element. Multiple elements in HTML can have the same class value. Also, it is mainly used to associate the styles written in the stylesheet with the HTML elements.</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
